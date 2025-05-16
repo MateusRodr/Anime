86d5b674becb61722b929eef0679c8f3f84922db
MateusRodr <mr5508113@gmail.com>
2025-05-16 15:08:10 -0300

    code debugado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
-import { AnimeModule } from './anime/anime.module';
 
 @Module({
-  imports: [ApiModule, AnimeModule],
+  imports: [ApiModule,],
   controllers: [AppController],
   providers: [AppService],
 })
